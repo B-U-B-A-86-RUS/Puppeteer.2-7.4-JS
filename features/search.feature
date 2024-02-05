67844795ebaaa97adcb927d7c86012f1d143ed4d
B-U-B-A-86-RUS <155003986+B-U-B-A-86-RUS@users.noreply.github.com>
2024-02-05 22:08:54 +0500

    4 fix

--- features/search.feature
+++ features/search.feature
@@ -1,31 +1,18 @@
 Feature: Service for Movie tickets order
-    
-    Feature Description:
         Scenario: Zveropolis pozitive
-        Given user is on page- "http://qamid.tmweb.ru/client/index.php"
+        Given user is on page1- "http://qamid.tmweb.ru/client/index.php"
         When The user selects the desired day
         When The user selects the desired movie
         When The user chooses a location
         When The user has booked tickets
         When The user has confirmed the booking of tickets
-        Then Is the QR code of the reservation visible
+        Then Is the QR code of the reservation visible1
     
-    Feature Description:
         Scenario: Mickey Mouse positive
-        Given user is on page- "http://qamid.tmweb.ru/client/index.php"
-        When The user selects the desired day
-        When The user selects the desired movie
-        When The user chooses a location
-        When The user has booked tickets
-        When The user has confirmed the booking of tickets
-        Then Is the QR code of the reservation visible
+        Given user is on page2- "http://qamid.tmweb.ru/client/index.php"
+        Then Is the QR code of the reservation visible2
     
-    Feature Description:
         Scenario: Mickey Mouse negative
-        Given user is on page- "http://qamid.tmweb.ru/client/index.php"
-        When The user selects the desired day
-        When The user selects the desired movie
+        Given user is on page3- "http://qamid.tmweb.ru/client/index.php"
         When The user selects the occupied place
-        When The user has booked tickets
-        When The user has confirmed the booking of tickets
-        Then Is the QR code of the reservation visible
+        Then Is the QR code of the reservation visible neg
--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -17,42 +17,17 @@ After(async function () {
   }
 });
 
-Given("user is on page- {string}", async function (string) {
-  // Открываем страницу
-  return await this.page.goto(`http://qamid.tmweb.ru/client/index.php`, {
-    setTimeout: 50000,
-  });
-});
-When("The user selects the desired day", async function () {
-  // Нужный день
-  return await clickElement(this.page, ".page-nav__day:last-child");
-});
-When("The user selects the desired movie", async function () {
-  //нужный фильм
-  return await clickElement(
-    this.page,
-    "body > main > section:nth-child(1) > div.movie-seances__hall > ul > li:nth-child(3) > a"
-  );
-});
-When("The user chooses a location", async function () {
-  return await clickElement(this.page, ".buying-scheme__chair_standart");
-});
-When("The user has booked tickets", async function () {
-  //
-  return await clickElement(this.page, ".acceptin-button");
-});
-When("The user has confirmed the booking of tickets", async function () {
-  //
-  return await clickElement(this.page, ".acceptin-button");
-});
-Then("Is the QR code of the reservation visible", async function (string) {
-  const actual = await getText(page, ".ticket__hint");
-  await expect(actual).toContain(
-    "Покажите QR-код нашему контроллеру для подтверждения бронирования."
-  );
-});
+Given("user is on page1- {string}", async function (string) {return await this.page.goto(`http://qamid.tmweb.ru/client/index.php`, {setTimeout: 50000,});});
+When("The user selects the desired day", async function () {return await clickElement(this.page, ".page-nav__day:last-child");});
+When("The user selects the desired movie", async function () {return await clickElement(this.page, "body > main > section:nth-child(1) > div.movie-seances__hall > ul > li:nth-child(3) > a");});
+When("The user chooses a location", async function () {return await clickElement(this.page, ".buying-scheme__chair_standart");});
+When("The user has booked tickets", async function () {return await clickElement(this.page, ".acceptin-button");});
+When("The user has confirmed the booking of tickets", async function () {return await clickElement(this.page, ".acceptin-button");});
+Then("Is the QR code of the reservation visible1", async function (string) 
+{const actual = await getText(page, ".ticket__hint");
+ await expect(actual).toContain("Покажите QR-код нашему контроллеру для подтверждения бронирования.");});
 
-Given("user is on page- {string}", async function (string) {
+Given("user is on page2- {string}", async function (string) {
   return await this.page.goto(`http://qamid.tmweb.ru/client/index.php`, {
     setTimeout: 50000,
   });
@@ -73,12 +48,12 @@ When("The user has booked tickets", async function () {
 When("The user has confirmed the booking of tickets", async function () {
   return await clickElement(this.page, ".acceptin-button");
 });
-Then("Is the QR code of the reservation visible", async function (string) {
+Then("Is the QR code of the reservation visible2", async function (string) {
   const actual = await getText(page, ".ticket__hint");
   await expect(actual).toContain("Приятного просмотра!");
 });
 
-Given("user is on page- {string}", async function (string) {
+Given("user is on page3- {string}", async function (string) {
   return await this.page.goto(`http://qamid.tmweb.ru/client/index.php`, {
     setTimeout: 50000,
   });
@@ -86,7 +61,7 @@ Given("user is on page- {string}", async function (string) {
 When("The user selects the desired day", async function () {
   return await clickElement(this.page, ".page-nav__day:last-child");
 });
-When("The user selects the desired movie", async function () {
+When("The user selects the desired movie2", async function () {
   return await clickElement(
     this.page,
     "body > main > section:nth-child(1) > div.movie-seances__hall > ul > li:nth-child(3) > a"
@@ -101,7 +76,7 @@ When("The user has booked tickets", async function () {
 When("The user has confirmed the booking of tickets", async function () {
   return await clickElement(this.page, ".acceptin-button");
 });
-Then("Is the QR code of the reservation visible", async function (string) {
+Then("Is the QR code of the reservation visible neg", async function (string) {
   const actual = await getText(page, ".ticket__hint");
-  await expect(actual).toContain("Выыбранно Вами место уже занято");
+  await expect(actual).toContain("Выбранное Вами место уже занято");
 });
